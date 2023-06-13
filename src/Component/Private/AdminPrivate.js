895802e03ec7e2305345c6cdad3453c048f8d438
Md Mitul Hossain <md.mitul.hossen99@gmail.com>
2023-06-14 00:14:01 +0600

    admin

--- src/Component/Private/AdminPrivate.js
+++ src/Component/Private/AdminPrivate.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 const AdminPrivate=({ user, children })=> { 
     
-  if (user && user?.role!=="admin") {
+  if (user?.role!=="admin") {
     return <Navigate to="/login" replace/>
   }
   else{
