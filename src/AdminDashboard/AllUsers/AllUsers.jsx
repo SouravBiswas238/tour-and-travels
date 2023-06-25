1f0627a02207643c1c316379a9d2e79cf992a7dc
Md. Shoaib Akhter Shifat <75001645+Bad-Luck@users.noreply.github.com>
2023-06-25 13:16:26 +0600

    AllUsers admin updated

--- src/AdminDashboard/AllUsers/AllUsers.jsx
+++ src/AdminDashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../UserContext/userContext';
+import UsersTable from './MiniComponent/UsersTable';
 
-function AllUsers() {
-  return (
-    <div>AllUsers</div>
-  )
-}
+
+const AllUsers = () => {
+
+    // fetch data from backend
+    const { userData } = useContext(UserContext);
+
+    console.log(userData)
+
+
+    return (
+        <div>
+            <h1>Users List</h1>
+            {/* <UsersTable data={ userData } /> */}
+        </div>
+    );
+};
 
 export default AllUsers
