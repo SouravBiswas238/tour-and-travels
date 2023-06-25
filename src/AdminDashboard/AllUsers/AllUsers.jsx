87ed6627bea4fe70ff4ccbd236d7e39268c8a5dc
Md. Shoaib Akhter Shifat <75001645+Bad-Luck@users.noreply.github.com>
2023-06-25 13:33:26 +0600

    AllUsers added in admin dashboard

--- src/AdminDashboard/AllUsers/AllUsers.jsx
+++ src/AdminDashboard/AllUsers/AllUsers.jsx
@@ -6,15 +6,15 @@ import UsersTable from './MiniComponent/UsersTable';
 const AllUsers = () => {
 
     // fetch data from backend
-    const { userData } = useContext(UserContext);
+    const { users } = useContext(UserContext);
 
-    console.log(userData)
+    // console.log(users)
 
 
     return (
         <div>
-            <h1>Users List</h1>
-            {/* <UsersTable data={ userData } /> */}
+            <h1 className='text-center text-xl my-3 font-bold'>Users Count: { users.length }</h1>
+            <UsersTable data={ users } />
         </div>
     );
 };
