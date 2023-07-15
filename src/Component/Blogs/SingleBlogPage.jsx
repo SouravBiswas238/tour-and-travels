15317b37574bbd4169fa4d9d1f505fd691a59577
Md. Shoaib Akhter Shifat <75001645+Bad-Luck@users.noreply.github.com>
2023-07-15 22:12:03 +0600

    coverImage updated on single blog page

--- src/Component/Blogs/SingleBlogPage.jsx
+++ src/Component/Blogs/SingleBlogPage.jsx
@@ -29,7 +29,7 @@ const SingleBlogPage = () => {
               {blog?.author && blog?.author[0]?.lastName}
             </span>
           </p>
-          <img src={img3} alt="" className="w-full mb-6 rounded-lg h-[400px]" />
+          <img src={blog?.coverImage} alt="" className="w-full mb-6 rounded-lg h-[400px]" />
           <p class="text-gray-700 leading-7 text-justify">{blog?.content}</p>
         </div>
       </div>
